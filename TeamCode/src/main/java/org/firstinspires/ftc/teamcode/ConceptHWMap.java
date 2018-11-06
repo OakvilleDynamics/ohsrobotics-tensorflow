7c0820a55bf65e3517622d1622f89fcf7b59b479
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2018-11-05 22:53:58 -0600

    Two OpModes for Concept

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptHWMap.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class ConceptHWMap {
+    // Hardware calls
+    public DcMotor  leftDrive   = null;
+    public DcMotor  rightDrive  = null;
+    public DcMotor  leftArm     = null;
+
+    public static final double ARM_UP_POWER    =  0.45 ;
+    public static final double ARM_DOWN_POWER  = -0.45 ;
+
+    // Hardware Map calls
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    // Constructor
+    // Used in opmodes for which hardware map is wich
+    public ConceptHWMap(){
+
+    }
+
+    // Init hardware
+    public void init(HardwareMap ahwMap) {
+        hwMap = ahwMap;
+
+        // Name and Init hardware
+        leftDrive  = hwMap.get(DcMotor.class, "left_drive");
+        rightDrive = hwMap.get(DcMotor.class, "right_drive");
+        leftArm    = hwMap.get(DcMotor.class, "left_arm");
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Power Zero
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        leftArm.setPower(0);
+
+        // Set all motors to run without encoders
+        // If you have motors that have and are used as encoders, change this
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptOpMode.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Pushbot: Teleop Tank", group="Pushbot")
+@Disabled
+public class ConceptOpMode extends OpMode{
+
+    // Vars
+    ConceptHWMap    robot       = new ConceptHWMap(); // use the class created to define a Pushbot's hardware
+
+    @Override
+    // Init
+    public void init() {
+        // Init Hardware
+        robot.init(hardwareMap);
+
+        // Init Telemetry
+        // I know its nothing but its nice to have and you cant hate me for it
+        telemetry.addData("ROBOT STATUS", "INITIALIZED");
+    }
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        double left;
+        double right;
+
+        // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+        left = -gamepad1.left_stick_y;
+        right = -gamepad1.right_stick_y;
+
+        robot.leftDrive.setPower(left);
+        robot.rightDrive.setPower(right);
+
+
+        // Use gamepad buttons to move the arm up (Y) and down (A)
+        if (gamepad1.y)
+            robot.leftArm.setPower(robot.ARM_UP_POWER);
+        else if (gamepad1.a)
+            robot.leftArm.setPower(robot.ARM_DOWN_POWER);
+        else
+            robot.leftArm.setPower(0.0);
+
+        // Send telemetry message to signify robot running;
+        telemetry.addData("left",  "%.2f", left);
+        telemetry.addData("right", "%.2f", right);
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+}
